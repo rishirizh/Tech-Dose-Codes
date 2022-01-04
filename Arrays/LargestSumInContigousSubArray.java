ee118b3c2960aa38eca489c23e578b51fc3174a8
rizh44 <57767657+rizh44@users.noreply.github.com>
2022-01-04 11:06:52 +0530

    Update LargestSumInContigousSubArray.java

--- Arrays/LargestSumInContigousSubArray.java
+++ Arrays/LargestSumInContigousSubArray.java
@@ -1,1 +1,27 @@
-c
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE,curr=0;
+        for(int i=0;i<n;i++){
+            curr += arr[i];
+            if(curr<arr[i]){
+                curr=arr[i];
+            }
+            if(curr>max){
+                max=curr;
+            }
+            // max = Math.max(max,curr);
+        }
+        System.out.print(max);
+		
+		
+		
+	}
+}
