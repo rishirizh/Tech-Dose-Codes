890b13bed355cd04044b7176072a5aca79f0272e
rizh44 <57767657+rizh44@users.noreply.github.com>
2022-01-05 19:30:03 +0530

    Update CountingBits.java

--- Bit Manipulation/CountingBits.java
+++ Bit Manipulation/CountingBits.java
@@ -1,3 +1,5 @@
+// normal solution
+
 class Solution {
     public int countSetBits(int n){
         int count = 0;
@@ -16,3 +18,33 @@ class Solution {
         
     }
 }
+
+
+// DP solution
+
+class Solution {
+    
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+        result[0] = 0;
+        if (n==0){
+            return result;
+        }
+        result[1] = 1;
+        for(int i=2;i<=n;i++){
+            if ((i&1)==1){
+                result[i] = result[i>>1]+1; 
+            }
+            else{
+                result[i] = result[i>>1];
+            }
+        }
+        return result;
+        
+    }
+}
+
+
+
+
+
